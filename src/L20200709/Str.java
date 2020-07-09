5766fbc4355979880b876f4301237091abf7cb19
TO-Java <tzachiomer@gmail.com>
2020-07-09 14:55:57 +0300

    20200709

---
+++ src/L20200709/Str.java
@@ -0,0 +1,70 @@
+package L20200709;
+
+public class Str {
+
+    public static void main(String[] args) {
+
+        String s1 = "Good Morning d";
+        System.out.println(s1);
+        String s2 = s1 + " " + s1;
+        System.out.println(s2);
+
+        char[] charArray = {'G', 'o', 'o', 'd'};
+        for (int i = 0; i < charArray.length; ++i) {
+            System.out.print(charArray[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < s1.length(); ++i) {
+            System.out.print(s1.charAt(i));
+        }
+        System.out.println();
+
+        System.out.println(s1.length());
+        System.out.println(s1.charAt(3));
+        System.out.println(s1.indexOf('d'));
+        System.out.println(s1.indexOf('d', 4));
+        System.out.println(s1.indexOf(" M"));
+        System.out.println(s1.indexOf("  "));
+        System.out.println(s1.substring(5, 12));
+        System.out.println(s1.substring(5, s1.length()));
+        System.out.println(s1.substring(5));
+        System.out.println(s1.substring(0, 4) + s1.substring(5));
+        System.out.println(extract(s1, 4));
+        System.out.println(substitute(s1, 4, 'X'));
+        System.out.println(s1);
+        System.out.println(count(s1, 'o'));
+        System.out.println(replaceAll(s1, 'o', 'X'));
+    }
+
+    public static String extract(String s, int idx) {
+        return s.substring(0, idx) + s.substring(idx + 1);
+    }
+
+    public static String substitute(String s, int idx, char chr) {
+        return s.substring(0, idx) + chr + s.substring(idx + 1);
+    }
+
+    public static int count(String s, char chr) {
+        int acc = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == chr) {
+                ++acc;
+            }
+        }
+        return acc;
+    }
+
+    public static String replaceAll(String s, char c1, char c2) {
+        String acc = "";
+        for (int i = 0; i < s.length(); ++i) {
+            char t = s.charAt(i);
+            if (t == c1) {
+                acc += c2;
+            } else {
+                acc += t;
+            }
+        }
+        return acc;
+    }
+}
